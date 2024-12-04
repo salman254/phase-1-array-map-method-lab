b5c6fd021dc58c8af371b24ba952803f857e6e32
Salman <salmaanmohamed700@gmail.com>
2024-12-04 09:19:47 +0300

    Array Map Method

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((tutorial) => {
+    return tutorial
+      .split(' ') 
+      .map((word) => word[0].toUpperCase() + word.slice(1)) 
+      .join(' '); 
+  });
+};
+
+console.log(titleCased());
